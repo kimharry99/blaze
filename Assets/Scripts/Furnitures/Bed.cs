8e4fb24ac744e34df47c534ac9b2c0973ea1422f
kimharry99 <kimharry99@korea.ac.kr>
2019-01-22 23:31:09 +0900

    update bed furniture

---
+++ Assets/Scripts/Furnitures/Bed.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bed : Furniture
+{
+    protected override FurnitureType type { get { return FurnitureType.Bed; } }
+
+    int usingTurn=1;
+
+    public override void OnUseButtonClicked()
+    {
+        GameManager.inst.StartTask(UseFurniture, usingTurn);
+    }
+
+    public void OnPlusButtonClicked()
+    {
+        usingTurn++;
+    }
+
+    public void OnMinusButtonClicked()
+    {
+        usingTurn = Mathf.Min(0,usingTurn-1);
+    }
+
+    private void UseFurniture()
+    {
+        GameManager.inst.Rest(usingTurn*4);
+    }
+
+}
