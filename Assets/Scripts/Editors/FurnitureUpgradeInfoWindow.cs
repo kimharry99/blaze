9680d4906ddbdf001b7e41d282a8ae74749f9d35
박보승 <pbs04054@gmail.com>
2019-01-22 18:38:42 +0900

    Furniture Upgrade Info Jsons, editor added

---
+++ Assets/Scripts/Editors/FurnitureUpgradeInfoWindow.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+public class FurnitureUpgradeInfoWindow : EditorWindow
+{
+	FurnitureType type;
+	int level;
+	int wood = 0;
+	int components = 0;
+	int parts = 0;
+
+	// Add menu named "My Window" to the Window menu
+	[MenuItem("Editors/가구 업그레이드 정보")]
+	static void Init()
+	{
+		// Get existing open window or if none, make a new one:
+		FurnitureUpgradeInfoWindow window = (FurnitureUpgradeInfoWindow)EditorWindow.GetWindow(typeof(FurnitureUpgradeInfoWindow));
+		window.name = "가구 업그레이드 정보 관리";
+		window.Show();
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Label("인덱싱 정보", EditorStyles.boldLabel);
+		type = (FurnitureType)EditorGUILayout.EnumPopup("가구 타입",type);
+		level = EditorGUILayout.IntSlider("Level", level, 0, 2);
+
+		GUILayout.Label("내용", EditorStyles.boldLabel);
+		wood = EditorGUILayout.IntField("Wood", wood);
+		components = EditorGUILayout.IntField("Components", components);
+		parts = EditorGUILayout.IntField("Parts", parts);
+
+		if (GUILayout.Button("불러오기"))
+		{
+			string json = File.ReadAllText(Application.dataPath + "/Resources/Jsons/FurnitureUpgradeInfo/" + type.ToString() + ".json");
+			FurnitureUpgradeInfo info = JsonUtility.FromJson<FurnitureUpgradeInfo>(json);
+			wood = info.wood[level];
+			components = info.components[level];
+			parts = info.parts[level];
+			AssetDatabase.Refresh();
+		}
+
+		if (GUILayout.Button("편집"))
+		{
+			string json = File.ReadAllText(Application.dataPath + "/Resources/Jsons/FurnitureUpgradeInfo/" + type.ToString() + ".json");
+			FurnitureUpgradeInfo info = JsonUtility.FromJson<FurnitureUpgradeInfo>(json);
+			info.wood[level] = wood;
+			info.components[level] = components;
+			info.parts[level] = parts;
+			File.WriteAllText(Application.dataPath + "/Resources/Jsons/FurnitureUpgradeInfo/" + type.ToString() + ".json", JsonUtility.ToJson(info, true));
+			AssetDatabase.Refresh();
+		}
+	}
+}
---
+++ Assets/Scripts/Generals/JsonHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class JsonHelper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		for (int i = 0; i < 6; ++i)
+		{
+			FurnitureUpgradeInfo info = new FurnitureUpgradeInfo();
+			info.type = (FurnitureType)i;
+			info.wood[0] = 0;
+			info.wood[1] = 0;
+			info.wood[2] = 0;
+			info.components[0] = 0;
+			info.components[1] = 0;
+			info.components[2] = 0;
+			info.parts[0] = 0;
+			info.parts[1] = 0;
+			info.parts[2] = 0;
+			File.WriteAllText(Application.dataPath + "/Resources/Jsons/FurnitureUpgradeInfo/" + ((FurnitureType)i).ToString() + ".json", JsonUtility.ToJson(info, true));
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[Serializable]
+public class FurnitureUpgradeInfo
+{
+	public FurnitureType type = FurnitureType.None;
+	public int[] wood = new int[3];
+	public int[] components = new int[3];
+	public int[] parts = new int[3]; 
+}
