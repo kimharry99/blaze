9e42fedf5db4bebae04ccb5a5abf6b0d7c365196
박보승 <pbs04054@gmail.com>
2019-01-25 17:01:46 +0900

    Resolve error

--- Assets/Scripts/Furnitures/Bed.cs
+++ Assets/Scripts/Furnitures/Bed.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Bed : Furniture
 {
-    protected override FurnitureType type { get { return FurnitureType.Bed; } }
+    public override FurnitureType type { get { return FurnitureType.Bed; } }
 
     int usingTurn=1;
 
