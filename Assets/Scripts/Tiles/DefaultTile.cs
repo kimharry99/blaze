de225ab31313b8a6a97de064968ac34f28ce263e
박보승 <pbs04054@gmail.com>
2019-01-26 03:06:39 +0900

    Add weather icon / furniture icon textures

--- Assets/Scripts/Tiles/DefaultTile.cs
+++ Assets/Scripts/Tiles/DefaultTile.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+/// <summary>
+/// Primitive class for all tiles in this game
+/// </summary>
 public abstract class DefaultTile : Tile
 {
 	#region Resources
