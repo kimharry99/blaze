a0354bd40f9a32c2a5a4c176a48db4bce4692f3f
박보승 <pbs04054@gmail.com>
2019-01-26 02:50:29 +0900

    Working on tilemap system

---
+++ Assets/Scripts/Managers/MapManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapManager : SingletonBehaviour<MapManager>
+{
+	public Tilemap tileMap;
+	public Vector3Int curPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		//AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<HomeTile>(),"Assets/Tiles/HomeTile.asset");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+		{
+			TileData data;
+			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector3Int cellPos = tileMap.WorldToCell(mousePos);
+		}
+    }
+	/*
+	private void MapMaker(int size)
+	{
+		tileMap.size = new Vector3Int(size,size,size);
+		for (int i = 0; i < size; ++i)
+			for (int j = 0; j < size; ++j)
+				for (int k = 0; k < size; ++k)
+				{
+					tileMap.SetTile(new Vector3Int(i,j,k),1)
+				}
+	}
+	*/
+}
---
+++ Assets/Scripts/Tiles/DefaultTile.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public abstract class DefaultTile : Tile
+{
+	#region Resources
+	public int Water { get; private set; }
+	public int Food { get; private set; }
+	public int Preserved { get; private set; }
+	public int Wood { get; private set; }
+	public int Components { get; private set; }
+	public int Parts { get; private set; }
+	#endregion
+
+
+	public void Init(int water, int food, int preserved, int wood, int components, int parts)
+	{
+
+	}
+
+	/// <summary>
+	/// Take Resources from tile. 
+	/// </summary>
+	public void TakeResources()
+	{
+		int water = Random.Range(0, Water + 1);
+		int food = Random.Range(0, Food + 1);
+		int preserved = Random.Range(0, Preserved + 1);
+		int wood = Random.Range(0, Wood + 1);
+		int components = Random.Range(0, Components + 1);
+		int parts = Random.Range(0, Parts + 1);
+
+		GameManager.inst.GetResource(water: water, food: food, preserved: preserved, wood: wood, components: components, parts: parts);
+		Water -= water;
+		Food -= food;
+		Preserved -= preserved;
+		Wood -= wood;
+		Components -= components;
+		Parts -= parts;
+	}
+
+	public abstract void OnVisited();
+}
