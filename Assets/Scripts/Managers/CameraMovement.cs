9218147c5c479ed177b137e1077aca6d07ffcc3f
박보승 <pbs04054@gmail.com>
2019-01-28 03:17:37 +0900

    Outdoor scene camera movement

---
+++ Assets/Scripts/Managers/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+	[SerializeField]
+	private float speed;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		if (Input.GetKey(KeyCode.D))
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		if (Input.GetKey(KeyCode.W))
+			transform.position += Vector3.up * speed * Time.deltaTime;
+		if (Input.GetKey(KeyCode.S))
+			transform.position += Vector3.down * speed * Time.deltaTime;
+		Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - Input.mouseScrollDelta.y, 1, 10);
+	}
+}
