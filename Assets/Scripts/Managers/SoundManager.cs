68d1df261e80ab74a76c203f91561d92fecfc073
박보승 <pbs04054@gmail.com>
2019-03-01 17:03:27 +0900

    사운드 매니저 추가

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SoundManager : SingletonBehaviour<SoundManager>
+{
+	public float volume = 0.5f;
+    public Dictionary<string, AudioClip> audioClips = new Dictionary<string, AudioClip>();
+
+    private void Awake()
+    {
+		SetStatic();
+		
+        AudioClip[] clips = Resources.LoadAll<AudioClip>("Sounds");
+        foreach (AudioClip clip in clips)
+        {
+			if (audioClips.ContainsKey(clip.name))
+                continue;
+            audioClips.Add(clip.name, clip);
+        }
+		
+    }
+
+    public void PlaySFX(GameObject target, AudioClip audio, float volume = -1)
+    {
+        if (audio == null)
+            return;
+		if (volume < 0)
+			volume = this.volume;
+        StartCoroutine(Play(target, audio, volume));
+    }
+
+    private IEnumerator Play(GameObject target, AudioClip audio, float volume)
+    {
+        AudioSource audioSource = target.AddComponent<AudioSource>();
+        audioSource.volume = volume;
+        audioSource.clip = audio;
+        audioSource.rolloffMode = AudioRolloffMode.Logarithmic;
+        audioSource.spatialBlend = 1;
+        audioSource.maxDistance = 200f;
+        audioSource.Play();
+        yield return new WaitForSeconds(audioSource.clip.length);
+        Destroy(audioSource);
+    }
+
+}
