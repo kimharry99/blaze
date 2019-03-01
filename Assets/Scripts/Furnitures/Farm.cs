a49500ca82ce55d5a70c1c6ede71eed53014b2fc
18김동민 <kimharry99@korea.ac.kr>
2019-03-02 01:31:25 +0900

    농장 스크립트 추가 + 농장 UI FarmPanel 추가 + 농장 UI 임시 이미지(농작물, 빈 슬롯) 추가

---
+++ Assets/Scripts/Furnitures/Farm.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum FarmState
+{
+    Idle,
+    Grow,
+    Harvest
+}
+
+enum FarmCrops
+{
+    Lettuce,
+    Carrot,
+    Bean,
+    Potato,
+    None = -1
+}
+public class Farm : Furniture
+{
+    public int[] turnLeft = new int[3];
+    public int[] turnTaken = new int[3];        //turn turnLeft/turnTaken
+    public int[] state = new int[3];
+    public int[] crops = new int[3];
+
+    public int selectedSlot = -1;   //no slot selected
+    private int option = 0;
+
+    public override void Init()
+    {
+        TurnManager.inst.OnTurnPassed += OnTurnPassed;
+    }
+
+    public override void OnTurnPassed(int turn)
+    {
+        for (int i = 0; i < level; ++i)
+        {
+            if (state[i] == (int)FarmState.Idle) { 
+                return;
+            }
+            turnLeft[i] -= turn;
+            if (turnLeft[i] <= 0)
+            {
+                state[i] = (int)FarmState.Harvest;
+            }
+            if (turnLeft[i] <= -192)
+            {
+            state[i] = (int)FarmState.Grow;
+            turnLeft[i] = turnTaken[i];
+            }
+        }
+    }
+
+    public void PlantCrops(int crop)
+    {
+        switch (crop)
+        {
+            case 0: //상추
+                if (!GameManager.inst.CheckResource(water: 20, food: 5))
+                    return;
+                if (level < 1)
+                    return;
+                GameManager.inst.UseResource(water: 20, food: 5);
+                turnTaken[selectedSlot] = Random.Range(10, 16);
+                turnLeft[selectedSlot] = turnTaken[selectedSlot];
+                crops[selectedSlot] = (int)FarmCrops.Lettuce;
+                break;
+            case 1: //당근
+                if (!GameManager.inst.CheckResource(water: 10, food: 20))
+                    return;
+                if (level < 1)
+                    return;
+                GameManager.inst.UseResource(water: 10, food: 20);
+                turnTaken[selectedSlot] = Random.Range(20, 26);
+                turnLeft[selectedSlot] = turnTaken[selectedSlot];
+                crops[selectedSlot] = (int)FarmCrops.Carrot;
+                break;
+            case 2: //콩
+                if (!GameManager.inst.CheckResource(water: 20, food: 10))
+                    return;
+                if (level < 2)
+                    return;
+                GameManager.inst.UseResource(water: 20, food: 10);
+                turnTaken[selectedSlot] = Random.Range(15, 21);
+                turnLeft[selectedSlot] = turnTaken[selectedSlot];
+                crops[selectedSlot] = (int)FarmCrops.Bean;
+                break;
+            case 3: //감자
+                if (!GameManager.inst.CheckResource(water: 40, food: 50))
+                    return;
+                if (level < 3)
+                    return;
+                GameManager.inst.UseResource(water: 40, food: 50);
+                turnTaken[selectedSlot] = Random.Range(50, 70);
+                turnLeft[selectedSlot] = turnTaken[selectedSlot];
+                crops[selectedSlot] = (int)FarmCrops.Potato;
+                break;
+            default:
+                Debug.Log("There is no Option for " + option.ToString());
+                return;
+        }
+        state[selectedSlot] = (int)FarmState.Grow;
+
+    }
+
+    public void CancelCrops()
+    {
+        turnTaken[selectedSlot] = 0;
+        turnLeft[selectedSlot] = 0;
+        crops[selectedSlot] = (int)FarmCrops.None;
+        state[selectedSlot] = (int)FarmState.Idle;
+    }
+
+    public void HarvestCrops(int crop)
+    {
+        switch (crop)
+        {
+            case (int)FarmCrops.Lettuce:
+                GameManager.inst.GetResource(food: 40);
+                break;
+            case (int)FarmCrops.Carrot:
+                GameManager.inst.GetResource(food: 70);
+                break;
+            case (int)FarmCrops.Bean:
+                GameManager.inst.GetResource(food: 70);
+                break;
+            case (int)FarmCrops.Potato:
+                GameManager.inst.GetResource(food: 200);
+                break;
+            default:
+                Debug.Log("There is no crop for " + crops[selectedSlot].ToString());
+                return;
+        }
+        CancelCrops();
+    }
+}
--- Assets/Scripts/Managers/HomeUIManager.cs
+++ Assets/Scripts/Managers/HomeUIManager.cs
@@ -17,10 +17,25 @@ public class HomeUIManager : SingletonBehaviour<HomeUIManager>
 	public Text bucketWaterText;
 	public Slider bucketWaterSlider;
 	public Button bucketHarvestButton;
-	#endregion
+    #endregion
 
-	#region Upgrade UI
-	public GameObject upgradePanel;
+    #region Farm UI Variables
+    [Header("Farm UI")]
+    public GameObject farmPanel;
+    public GameObject cropSelectPanels;
+    public Button[] plantCropButtons = new Button[4];
+    public Sprite[] cropImages = new Sprite[4];
+    public Image[] slotImages = new Image[3];
+    public Image[] fillImages = new Image[3];
+    public Text[] farmTexts = new Text[3];
+    public Slider[] farmSliders = new Slider[3];
+    public Button[] farmCropSelectButtons = new Button[3];
+    public Button[] farmHarvestButtons = new Button[3];
+    public Button[] farmCancelButtons = new Button[3]; 
+    #endregion
+
+    #region Upgrade UI
+    public GameObject upgradePanel;
 	public Text woodText;
 	public Text componentsText;
 	public Text partsText;
@@ -126,5 +141,114 @@ public class HomeUIManager : SingletonBehaviour<HomeUIManager>
 		bucket.HarvestWater();
 		OpenBucketPanel();
 	}
-	#endregion
+    #endregion
+
+    #region Farm UI Functions
+    public void OpenFarmPanel()
+    {
+        Farm farm = (Farm)gm.furnitures["Farm"];
+        for(int i = 0; i < 3; i++)
+        {
+            if (i < farm.level)
+            {
+                farmHarvestButtons[i].interactable = farm.state[i] == 2;    //Grow
+                farmCancelButtons[i].interactable = farm.state[i] != 0;     //Idle
+                farmCropSelectButtons[i].interactable = farm.state[i] == 0; //Idle
+                if (farm.state[i] == 0)
+                {
+                    farmTexts[i].text = "0/0";
+                    farmSliders[i].value = 0;
+                }
+                else if (farm.state[i] == 1)
+                {
+                    farmTexts[i].text = farm.turnLeft[i].ToString() + "/" + farm.turnTaken[i].ToString();
+                    farmSliders[i].value = farm.turnLeft[i] / (float)farm.turnTaken[i];
+                }
+                else
+                {
+                    farmTexts[i].text= (-1*farm.turnLeft[i]).ToString() + "/" + 196.ToString();
+                    farmSliders[i].value = 0;
+                }
+            }
+        }
+        farmPanel.SetActive(true);
+    }
+
+    public void CloseFarmPanel()
+    {
+        Debug.Log("click");
+        farmPanel.SetActive(false);
+    }
+
+    public void OpenCropSelectPanel(int slot)
+    {
+        if (!cropSelectPanels.activeSelf)
+        {
+            Farm farm = (Farm)gm.furnitures["Farm"];
+            for (int i = 0; i < 4; i++)
+            {
+                switch (i)
+                {
+                    case 0: //상추
+                        plantCropButtons[i].interactable = GameManager.inst.CheckResource(water: 20, food: 5)&& i <= farm.level; 
+                        break;
+                    case 1: //당근
+                        plantCropButtons[i].interactable = GameManager.inst.CheckResource(water: 10, food: 20) && i <= farm.level;
+                        break;
+                    case 2: //콩
+                        plantCropButtons[i].interactable = GameManager.inst.CheckResource(water: 20, food: 10) && i <= farm.level;
+                        break;
+                    case 3: //감자
+                        plantCropButtons[i].interactable = GameManager.inst.CheckResource(water: 40, food: 50) && i <= farm.level;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            cropSelectPanels.GetComponent<RectTransform>().localPosition = new Vector3(-150 + slot * 450,0, 0);
+
+            farm.selectedSlot = slot;
+            cropSelectPanels.SetActive(true);
+        }
+    }
+
+    public void CloseCropSelectPanel(int slot)
+    {
+        Debug.Log("click");
+        Farm farm = (Farm)gm.furnitures["Farm"];
+        farm.selectedSlot = -1;
+        cropSelectPanels.SetActive(false);
+    }
+
+    
+    public void Farm_PlantCrop(int crop)
+    {
+        Debug.Log("click");
+        tm.UseTurn(8);
+        Farm farm = (Farm)gm.furnitures["Farm"];
+        slotImages[farm.selectedSlot].GetComponent<Image>().sprite = cropImages[crop];
+        fillImages[farm.selectedSlot].GetComponent<Image>().sprite = cropImages[crop];
+        farm.PlantCrops(crop);
+        CloseCropSelectPanel(farm.selectedSlot);
+        CloseFarmPanel();
+    }
+    public void Farm_HarvestCrop(int slot)
+    {
+        Farm farm = (Farm)gm.furnitures["Farm"];
+        farm.selectedSlot = slot;
+        tm.UseTurn(4);
+        slotImages[farm.selectedSlot].GetComponent<Image>().sprite = null;
+        farm.HarvestCrops(farm.crops[farm.selectedSlot]);
+        OpenFarmPanel();
+    }
+
+    public void Farm_CancelSlot(int slot)
+    {
+        Farm farm = (Farm)gm.furnitures["Farm"];
+        farm.selectedSlot = slot;
+        slotImages[farm.selectedSlot].GetComponent<Image>().sprite = null;
+        farm.CancelCrops();
+        OpenFarmPanel();
+    }
+    #endregion
 }
