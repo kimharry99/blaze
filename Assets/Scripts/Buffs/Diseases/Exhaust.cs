f32e356d2518f42bf58154739bca3aad820eb236
박보승 <pbs04054@gmail.com>
2019-03-04 16:42:14 +0900

    탈진 상태이상 추가

---
+++ Assets/Scripts/Buffs/Diseases/Exhaust.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exhaust : Buff
+{
+	public override bool IsActivated
+	{
+		get { return GameManager.inst.Energy <= 0; }
+	}
+
+	public override void Apply(int turn)
+	{
+		GameManager.inst.turnPenaltyConst += 0.5f;
+	}
+}
