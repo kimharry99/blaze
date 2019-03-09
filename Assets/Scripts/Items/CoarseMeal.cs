203adf502b09f6e65bd1274d49691d165720722f
박보승 <pbs04054@gmail.com>
2019-03-09 10:00:21 +0900

    조악한 식사 섭취시 식중독 발생

---
+++ Assets/Scripts/Items/CoarseMeal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoarseMeal : Food
+{
+	public override void Use()
+	{
+		base.Use();
+		float rand = Random.Range(0f, 100f);
+		if (rand < 10)
+		{
+			((Disease)GameManager.inst.GetBuff("FoodPoisoning")).AddNewDisease();
+		}
+	}
+}
